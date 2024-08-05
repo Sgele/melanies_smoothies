ed6bc09bc0fbbe3b3b70877bed84ce81efa17459
Simona Gelzinyte <90323260+Sgele@users.noreply.github.com>
2024-08-05 13:58:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,4 @@ if ingredients_list:
 # New section to Display fruityvice nutririon information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
+st. text(fruityvice_response.json())
